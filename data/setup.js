a5b2426c198c58c8f79a183d1d9eeec58bb6cb5c
Dylan Floyd <floyd800@gmail.com>
2022-02-23 13:33:05 -0800

    add some recipes, recipe db insertion, and GET /recipes route

--- data/setup.js
+++ data/setup.js
@@ -1,5 +1,6 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
+/* 
 module.exports = (pool) => {
   return fs
     .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
@@ -19,4 +20,37 @@ module.exports = (pool) => {
         console.error('❌ Error: ' + error.message);
       }
     });
+};
+*/
+
+module.exports = async (pool) => {
+  const setupFile = fs.readFileSync(`${__dirname}/../sql/setup.sql`);
+  await pool.query(setupFile.toString());
+  console.log('Table setup complete');
+  const recipeFile = fs.readFileSync(`${__dirname}/./recipes.json`);
+  const recipes = JSON.parse(recipeFile.toString());
+  await Promise.all(recipes.map(async (recipe) => {
+    await pool.query(`
+      INSERT INTO recipe (
+        name,
+        description,
+        ingredients,
+        instructions,
+        tags,
+        servings,
+        image,
+        total_time)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    `, [
+      recipe.name,
+      recipe.description,
+      recipe.ingredients,
+      recipe.instructions,
+      recipe.tags,
+      recipe.time.total,
+      recipe.servings,
+      recipe.image
+    ]);
+  }));
+  console.log('Recipe data loaded.');
 };
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,28 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS recipe CASCADE;
+DROP TABLE IF EXISTS app_user CASCADE;
+
+CREATE TABLE app_user (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  password_hash TEXT NOT NULL,
+  show_user_content BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE recipe (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  owner_id BIGINT REFERENCES app_user(id),
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  ingredients TEXT[] NOT NULL,
+  instructions TEXT[] NOT NULL,
+  tags TEXT[] NOT NULL,
+  servings TEXT NOT NULL,
+  image TEXT NOT NULL,
+  total_time TEXT,
+  is_public BOOLEAN DEFAULT FALSE,
+  source_url TEXT, -- MAKE NOT NULL LATER ONCE WE FIX RECIPE DATA
+  rating FLOAT NOT NULL DEFAULT 0.0,
+  ratings_count INT NOT NULL DEFAULT 0
+);
+
