caec0e996d738014d5699df7aaaa5cb327c8ed83
Dylan Floyd <floyd800@gmail.com>
2022-02-24 12:27:36 -0800

    add protections to deployed DBs, remove imageless recipes, update data/setup.js to match to table scheme, add some seed data to tables

--- data/setup.js
+++ data/setup.js
@@ -1,28 +1,5 @@
 const fs = require('fs');
 
-/* 
-module.exports = (pool) => {
-  return fs
-    .readFile(`${__dirname}/../sql/setup.sql`, { encoding: 'utf-8' })
-    .then((sql) => pool.query(sql))
-    .then(() => console.log('✅ Database setup complete!'))
-    .catch((error) => {
-      const dbNotFound = error.message.match(/database "(.+)" does not exist/i);
-
-      if (dbNotFound) {
-        const [err, db] = dbNotFound;
-        console.error('❌ Error: ' + err);
-        console.info(
-          `Try running \`createdb -U postgres ${db}\` in your terminal`
-        );
-      } else {
-        console.error(error);
-        console.error('❌ Error: ' + error.message);
-      }
-    });
-};
-*/
-
 module.exports = async (pool) => {
   const production = process.env.NODE_ENV === 'production';
   const resetOkay = process.env.DB_RESET_OK === 'true';
@@ -33,6 +10,7 @@ module.exports = async (pool) => {
   const setupFile = fs.readFileSync(`${__dirname}/../sql/setup.sql`);
   await pool.query(setupFile.toString());
   console.log('Table setup complete');
+
   const recipeFile = fs.readFileSync(`${__dirname}/./recipes.json`);
   const recipes = JSON.parse(recipeFile.toString());
   await Promise.all(recipes.map(async (recipe) => {
@@ -54,9 +32,9 @@ module.exports = async (pool) => {
         recipe.description,
         recipe.instructions,
         recipe.tags,
-        recipe.time.total,
         recipe.servings,
         recipe.image,
+        recipe.time.total,
         recipe.sourceURL
       ]);
 
--- sql/setup.sql
+++ sql/setup.sql
@@ -34,29 +34,29 @@ CREATE TABLE recipe (
 CREATE TABLE ingredient (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   description TEXT NOT NULL,
-  recipe_id BIGINT REFERENCES recipe(id)
+  recipe_id BIGINT NOT NULL REFERENCES recipe(id)
 );
 
 CREATE TABLE shopping_list (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id BIGINT REFERENCES app_user(id),
-  ingredient_id BIGINT REFERENCES ingredient(id),
+  user_id BIGINT NOT NULL REFERENCES app_user(id),
+  ingredient_id BIGINT NOT NULL REFERENCES ingredient(id),
   is_checked BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE cookbook (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  recipe_id BIGINT REFERENCES recipe(id),
-  user_id BIGINT REFERENCES app_user(id)
+  recipe_id BIGINT NOT NULL REFERENCES recipe(id),
+  user_id BIGINT NOT NULL REFERENCES app_user(id)
 );
 
 CREATE TYPE day_enum AS ENUM ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
 
 CREATE TABLE day_planner (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  recipe_id BIGINT REFERENCES recipe(id),
-  day day_enum,
-  user_id BIGINT REFERENCES app_user(id)
+  recipe_id BIGINT NULL NULL REFERENCES recipe(id),
+  day day_enum NOT NULL,
+  user_id BIGINT NOT NULL REFERENCES app_user(id)
 );
 
 
@@ -74,7 +74,5 @@ INSERT INTO ingredient (recipe_id, description) VALUES (2, 'dog');
 
 INSERT INTO app_user (username, password_hash) VALUES ('bob', 'bob');
 
-INSERT INTO planner_day (user_id) VALUES (1); -- will be the reference for monday
-
-INSERT INTO day_recipe (recipe_id, planner_day_id) VALUES (1, 1); -- add banana bread to monday
-INSERT INTO day_recipe (recipe_id, planner_day_id) VALUES (2, 1); -- add corndog to monday
+INSERT INTO day_planner (recipe_id, day, user_id) VALUES (1, 'monday', 1);
+INSERT INTO day_planner (recipe_id, day, user_id) VALUES (2, 'monday', 1); 
