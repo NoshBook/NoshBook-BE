f16c336e5c4f8af8bd831531fb6d750ba5f6dd37
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-24 15:32:22 -0800

    Add tests for planner post and get recipes routes

--- __tests__/planners.test.js
+++ __tests__/planners.test.js
@@ -4,8 +4,49 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Planner = require('../lib/models/Planner');
 
-const agent = request.agent(app);
+const testPlanner = {
+  day: 'tuesday',
+  recipeId: 3,
+  userId: 1,
+};
 
 describe('planner routes', () => {
-  it('should create a new planner day recipe association', async () => {});
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should create a new planner day recipe association', async () => {
+    const res = await request(app).post('/api/v1/planners').send(testPlanner);
+
+    expect(res.body).toEqual({
+      day: 'tuesday',
+      recipeId: '3',
+      userId: '1',
+      id: expect.any(String),
+    });
+  });
+
+  it('should get all recipes in planner for a given user', async () => {
+    const res = await request(app).get('/api/v1/planners/1');
+
+    expect(res.body).toEqual([
+      {
+        day: 'monday',
+        recipes: [
+          {
+            id: 1,
+            name: 'banana bread',
+          },
+          {
+            id: 2,
+            name: 'corndog',
+          },
+        ],
+      },
+    ]);
+  });
 });
