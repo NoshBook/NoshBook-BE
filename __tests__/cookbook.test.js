e71a4011dae19389825fe0ac4226a6ddf45d645c
Michael Rider <mikepdxrider@gmail.com>
2022-02-25 14:39:41 -0800

    adds passing test for add recipe to cookbook route

---
+++ __tests__/cookbook.test.js
@@ -0,0 +1,36 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+//declare agent to persist cookies
+const agent = request.agent(app);
+
+const mockUser = {
+  username: 'bob',
+  password: 'bob',
+};
+
+describe('recipe routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should return an array of recipes', async () => {
+    // login user to retrieve a cookie
+    await agent.post('/api/v1/users/sessions').send(mockUser);
+
+    const addRecipeToCookbookRes = await agent.get('/api/v1/cookbooks/add/1');
+    const actual = addRecipeToCookbookRes.body;
+
+    expect(actual).toEqual({
+      id: expect.any(String),
+      userId: '1',
+      recipeId: '1',
+    });
+  });
+});
