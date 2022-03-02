ef9df1d109925a0fc15e3cc4461e2b86fdab3649
Dylan Floyd <floyd800@gmail.com>
2022-03-01 18:00:42 -0800

    fix recipes.json loading so the source url actually makes it to the db

--- data/setup.js
+++ data/setup.js
@@ -36,17 +36,17 @@ module.exports = async (pool) => {
     await loadTestData(pool);
   }
 
-  const recipeFile = fs.readFileSync(`${__dirname}/./recipes.json`);
+  const recipeFile = fs.readFileSync(`${__dirname}/recipes.json`);
   const recipes = JSON.parse(recipeFile.toString());
   await Promise.all(recipes.map(async (recipe) => {
     try {
       const { rows } = await pool.query(`
         INSERT INTO recipe (name, description, instructions, tags,
-        servings, image, total_time, owner_id)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        servings, image, total_time, owner_id, source_url, is_public)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, true)
         RETURNING *;
       `, [recipe.name, recipe.description, recipe.instructions, recipe.tags, recipe.servings,
-        recipe.image, recipe.totalTime, recipe.userId]);
+        recipe.image, recipe.totalTime, recipe.userId, recipe.sourceURL]);
 
       await Promise.all(recipe.ingredients.map(async (ingredient) => await pool.query(`
         INSERT INTO ingredient (description, recipe_id)
